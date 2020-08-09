cfbeb0b98aa7046e5eca7a8279b2b50e37c3fa45
Kevin <stradtkt22@gmail.com>
2020-08-08 23:36:41 -0400

    Added a modal

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,27 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './PlaceItem.css';
 import Card from '../../shared/components/UIElements/Card';
+import Button from './../../shared/components/FormElements/Button';
+import Modal from './../../shared/components/UIElements/Modal';
 
 const PlaceItem = props => {
+    const [showMap, setShowMap] = useState(false);
+    const openMap = () => setShowMap(true);
+    const closeMap = () => setShowMap(false);
     return (
-        <li className="place-item">
-            <Card className="place-item__content">
-                <div className="place-item__image">
-                    <img src={props.image} alt={props.title} />
-                </div>
-                <div className="place-item__info">
-                    <h2>{props.title}</h2>
-                    <h3>{props.address}</h3>
-                    <p>{props.description}</p>
-                </div>
-                <div className="place-item__actions">
-                    <button>VIEW ON MAP</button>
-                    <button>EDIT</button>
-                    <button>DELETE</button>
-                </div>
-            </Card>
-        </li>
+        <React.Fragment>
+            <Modal 
+                show={showMap} 
+                onCancel={closeMap} 
+                header={props.address} 
+                contentClass="place-item__modal-content"
+                footerClass="place-item__modal-actions"
+                footer={<Button onClick={closeMap}>CLOSE</Button>}>
+                    <div className="map-container">
+                        <h1>The Map</h1>
+                    </div>
+            </Modal>
+            <li className="place-item">
+                <Card className="place-item__content">
+                    <div className="place-item__image">
+                        <img src={props.image} alt={props.title} />
+                    </div>
+                    <div className="place-item__info">
+                        <h2>{props.title}</h2>
+                        <h3>{props.address}</h3>
+                        <p>{props.description}</p>
+                    </div>
+                    <div className="place-item__actions">
+                        <Button inverse onClick={openMap}>VIEW ON MAP</Button>
+                        <Button to={`/places/${props.id}`}>EDIT</Button>
+                        <Button danger>DELETE</Button>
+                    </div>
+                </Card>
+            </li>
+        </React.Fragment>
     );
 };
 
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PlaceList from '../components/PlaceList';
+import { useParams } from 'react-router-dom';
 
 const PLACES = [
     {
@@ -38,8 +39,10 @@ const PLACES = [
 ];
 
 const UserPlaces = () => {
+    const userId = useParams().userId;
+    const loadedPlaces = PLACES.filter(place => place.creator === userId);
     return (
-        <PlaceList items={PLACES} />
+        <PlaceList items={loadedPlaces} />
     );
 };
 
